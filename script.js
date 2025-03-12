2ff7b2dd6bfbf81340645f26154431de4573e163
Demo <naiks0234@gmail.com>
2025-03-12 10:06:24 +0530

    add animation

--- script.js
+++ script.js
@@ -14,9 +14,6 @@ function fun(user_choice) {
   let user = parseInt(user_score.innerText);
   let cmp = parseInt(computer_score.innerText);
 
-  ele1.src = `${user_choice}.png`;
-  ele2.src = `${cmp_Choice}.png`;
-
   if (user_choice === cmp_Choice) {
     ele3.innerHTML = "Draw";
   } else if (
@@ -32,19 +29,32 @@ function fun(user_choice) {
     cmp += 1;
     computer_score.innerHTML = cmp;
   }
-
-  animate();
+  animate(user_choice, cmp_Choice);
 }
 
-function animate() {
+function animate(user_choice, cmp_Choice) {
   let ele1 = document.getElementById("user_display");
   let ele2 = document.getElementById("computer_display");
 
-  ele1.style.animation = "none";
-  ele2.style.animation = "none";
 
-  setTimeout(() => {
-    ele1.style.animation = "user_scale 2s";
-    ele2.style.animation = "computer_scale 2s";
-  }, 10);
+  ele1.classList.remove("shake");
+  ele2.classList.remove("shake");
+
+
+  ele1.classList.add("shake");
+  ele2.classList.add("shake");
+
+
+  ele1.addEventListener("animationend", function changeUserImage() {
+    ele1.classList.remove("shake"); 
+    ele1.src = `${user_choice}.png`;
+    ele1.removeEventListener("animationend", changeUserImage);
+  });
+
+   
+  ele2.addEventListener("animationend", function changeComputerImage() {
+    ele2.classList.remove("shake");
+    ele2.src = `${cmp_Choice}.png`;
+    ele2.removeEventListener("animationend", changeComputerImage);
+  });
 }
